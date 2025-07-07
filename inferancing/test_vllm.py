9a6ec4cd8c87e0ed3b41d8056724ce041c2ecebe
Jacob <jacobedwardhaig@gmail.com>
2025-07-06 23:22:18 -0400

    Split the repo

--- inferancing/test_vllm.py
+++ inferancing/test_vllm.py
@@ -9,11 +9,8 @@ import asyncio
 
 print("Loading LLM...")
 
-# sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=32768)
-# llm = LLM(model="microsoft/Phi-4-mini-reasoning", gpu_memory_utilization=0.5)
-
-sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=8192)
-llm = LLM(model="microsoft/Phi-3.5-mini-instruct", gpu_memory_utilization=0.85, max_model_len=3540)
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=32768)
+llm = LLM(model="microsoft/Phi-4-mini-reasoning", gpu_memory_utilization=0.85)
 
 # sampling_params = SamplingParams(temperature=0.7, top_p=0.80, top_k=20,min_p=0.0, max_tokens=2048)
 # llm = LLM(model="Menlo/Jan-nano-128k", gpu_memory_utilization=0.9)
